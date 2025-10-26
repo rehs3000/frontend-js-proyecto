b67c82c9f9aa0fb5be1791c5cc8dfa232fc34190
Raúl Hernández <rehs3000@gmail.com>
2025-10-25 23:42:36 -0300

    javascript modifcado

--- js/index.js
+++ js/index.js
@@ -2,14 +2,24 @@ let   a = 3;
 let  b = 5;
  console.log(a+b);
 
-let nombre = prompt("Ingresar tu nombre: ")
-alert(nombre);
-let formatear = confirm('Estas seguri que desea formatear ? ')
-console.log(formatear);
-console.warn("probando el warn")
-console.error('tenemos un problema')
-const productos = ["Termostato", "Cámara WiFi", "Bombilla Smart", "Sensor de movimiento"];
+// let nombre = prompt("Ingresar tu nombre: ")
+// alert(nombre);
+// let formatear = confirm('Estas seguri que desea formatear ? ')
+// console.log(formatear);
+// console.warn("probando el warn")
+// console.error('tenemos un problema')
+// const productos = ["Termostato", "Cámara WiFi", "Bombilla Smart", "Sensor de movimiento"];
 
 console.table(productos)
 
 let empresa = "tienda smart";
+
+const persona = {
+    nombre: "Laura",
+    apellido: "Hernandez",
+    direccion: {calle: "Arias",numero : 129},
+    carrera: {titulo: programadora, enCurso: true, cantMateriasAprobadas: 4, materiasAprobadas: ["Programacion ","estructura","introduccion a la información"]}
+}
+console.log(persona.nombre)
+console.log(persona.apellido)
+console.log(`recion ${persona.direccion.calle} ${persona.direccion.numero}`)
