384b8a93be0691a7cbf952fed7ad6afa0733590b
Raúl Hernández <rehs3000@gmail.com>
2025-10-25 22:21:08 -0300

    agregamos el JS

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let   a = 3;
+let  b = 5;
+ console.log(a+b);
+
+let nombre = prompt("Ingresar tu nombre: ")
+alert(nombre);
+let formatear = confirm('Estas seguri que desea formatear ? ')
+console.log(formatear);
+console.warn("probando el warn")
+console.error('tenemos un problema')
+const productos = ["Termostato", "Cámara WiFi", "Bombilla Smart", "Sensor de movimiento"];
+
+console.table(productos)
+
+let empresa = "tienda smart";
